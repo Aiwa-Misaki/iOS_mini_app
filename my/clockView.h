990cd55aff7000d68ef3321430fcd128d7a80b52
AiwaMisaki <zyxwhisper@qq.com>
2021-07-20 16:45:15 +0800

    增加了图形界面选择时间长度的功能

--- my/clockView.h
+++ my/clockView.h
@@ -13,11 +13,24 @@ NS_ASSUME_NONNULL_BEGIN
 #define clock_radis 350
 #define bear_width 50
 
+typedef enum{
+    NOT_START=0,
+    COUNTING=1,
+    STOPPED=2,
+    ENDED=3
+} CLOCK_STATUS;//计时状态
+
 @interface clockView : UIView
-@property (nonatomic,assign) CGFloat arc;
+@property (nonatomic,assign) CGFloat arc;//弧度,3:00为零点，顺时针为正
+@property (nonatomic,assign) NSInteger remainSec;//剩余的秒数
+@property (nonatomic,assign) CLOCK_STATUS status;//计时状态
+@property (nonatomic,assign) UILabel* timeDisplay;//正中间显示的时间
+
 - (void)drawRect:(CGRect)rect;
 
--(void)setArc:(CGFloat)degree;
+- (void)setArc:(CGFloat)degree;//设置度数并重绘
+
+- (void)initUI;//初始化按钮和数字
 
 @end
 
--- my/clockView.m
+++ my/clockView.m
@@ -12,7 +12,7 @@
 
 @implementation clockView
 
--(clockView *)init{
+- (clockView *)init{
     self=[super init];
     [self setWidth:clock_radis];
     [self setHeight:clock_radis];
@@ -23,8 +23,8 @@
 
 - (void)drawRect:(CGRect)rect {
 
-    UIColor *cutePink=[UIColor colorWithRed:221/255.0 green:199/255.0 blue:222/255.0 alpha:10];
-    [[UIColor systemGrayColor] setFill];
+    UIColor *cutePink=[UIColor colorWithRed:221/255.0 green:199/255.0 blue:222/255.0 alpha:0];
+    [cutePink setFill];
     UIRectFill(rect);
     
     CGContextRef context = UIGraphicsGetCurrentContext();
@@ -32,15 +32,28 @@
     CGContextAddArc(context, self.center.x-self.frame.origin.x,self.center.y-self.frame.origin.y, rect.size.width/2-bear_width, -0.5*PI,self.arc , 0);
     [[UIColor whiteColor] set];
       CGContextStrokePath(context);
+    
+    [self initUI];
 
 }
 
--(void)setArc:(CGFloat)arc{//设置角度
+- (void)setArc:(CGFloat)arc{//设置角度
     NSLog(@"%@", [NSString stringWithFormat:@"现在时钟的角度为%f",self.arc*(180/PI)]);
     _arc=(arc-PI/2);
     if(_arc>2*PI)
         _arc-=2*PI;
     [self setNeedsDisplay];
+}
+
+- (void)initUI{//初始化按钮和数字
+    CGContextRef ctx=UIGraphicsGetCurrentContext();
+    NSString *str=[NSString stringWithFormat:@"趣味儿童与i哦怕是对方过后就哭了自行车v不能买"];//时间标签
+    //字体属性
+    NSDictionary *dict=[NSDictionary dictionaryWithObjectsAndKeys:
+                               [UIFont fontWithName:@"DBLCDTempBlack" size:50], NSFontAttributeName,
+                               [UIColor colorWithRed:114/255.0f green:128/255.0f blue:137/255.0f alpha:1.0f], NSForegroundColorAttributeName, nil];
+    [str drawAtPoint:CGPointMake(self.center.x, self.center.y) withAttributes:dict];
+    NSLog(@"数字绘制完成！");
 }
 
 @end
