7e6f17f5b7f1a733a81dcd037db889287832938a
AiwaMisaki <zyxwhisper@qq.com>
2021-07-26 11:01:02 +0800

    使用纯代码添加了设置按钮

--- my/TomatoViewController.m
+++ my/TomatoViewController.m
@@ -7,9 +7,13 @@
 
 #import "TomatoViewController.h"
 #import "clockView.h"
+#import "myButton.h"
 #include "PopupView.h"
 
 @interface TomatoViewController ()
+@property (nonatomic, strong) UIButton *setButton;//这个可以封装一下
+@property (nonatomic, strong) UIButton *startButton;
+@property (nonatomic, strong) UIButton *pulseButton;
 @property (nonatomic, strong) clockView  *clockSelect;
 @end
 
@@ -27,6 +31,25 @@
     self.clockSelect.center=self.view.center;
     [self.view addSubview:self.clockSelect];
     [self.clockSelect setNeedsDisplay];
+    [self initButton:_setButton];
+
+}
+-(void)initButton:(UIButton*)button{
+    button=[UIButton buttonWithType:UIButtonTypeRoundedRect];//圆角按钮
+    button.layer.cornerRadius =15;
+    //设置属性
+    CGPoint center=CGPointMake(0.5*self.view.bounds.size.width, 0.75*self.view.bounds.size.height);
+    NSLog(@"%@",NSStringFromCGPoint(center));
+    CGFloat height=65;
+    CGFloat width=260;
+    button.frame=CGRectMake(center.x-0.5*width, center.y-0.5*height, width, height);
+    button.backgroundColor=[UIColor colorWithRed:155/255.0 green:144/255.0 blue:194/255.0 alpha:1.0];
+    [button setTitle:@"SET" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [button setEnabled:YES];
+    [self.view addSubview:button];
+    [button setNeedsDisplay];
+    NSLog(@"button init!");
 }
 // 触摸移动
 - (void)touchesMoved:(UITapGestureRecognizer *)gesture{
@@ -53,18 +76,14 @@
             arc=2*PI-atan(fabs(deltax/deltay));
         }
         CGFloat degree=arc*(180/PI);
-//        NSLog(@"当前与12:00夹角为%.1f度",degree);
         _clockSelect.degree=degree;
         [self.clockSelect setArc:arc];
-        
         //这里计算sin和cos值，传入和12:00的角度（顺时针为正，[0,360)）
     }else if (gesture.state==UIGestureRecognizerStateEnded){
         //这里修改arc和degree，变成时间是5的倍数
-        //封装得不好，为什么要传两个一样的参数
         //五分钟为30度
         _clockSelect.degree=roundf(_clockSelect.degree/30.0)*30;
         [_clockSelect setArc:_clockSelect.degree/(360/(2*PI))];
-//        NSLog(@"此处的arc为%.2f",_clockSelect.arc);
         //这边重绘
     }
     
@@ -74,7 +93,5 @@
     UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(touchesMoved:)];
     [self.view addGestureRecognizer:panGesture];
 }
-
-
 
 @end
---
+++ my/myButton.m
@@ -0,0 +1,18 @@
+//
+//  myButton.m
+//  my
+//
+//  Created by duck on 2021/7/24.
+//
+
+#import "myButton.h"
+
+@implementation myButton
+
+
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:_position];
+}
+
+
+@end
